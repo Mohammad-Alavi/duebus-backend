2519d964f65c5f24fa0428b98218a8f34231cddb
Moslem Deris <moslem.deris@gmail.com>
2019-07-12 17:46:38 +0430

    Add `Sector` settings in backend

---
+++ plugins/denora/duebus/models/Sector.php
@@ -0,0 +1,29 @@
+<?php namespace Denora\Duebus\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Sector extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    
+    /*
+     * Disable timestamps by default.
+     * Remove this line if timestamps are defined in the database table.
+     */
+    public $timestamps = false;
+
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'denora_duebus_sectors';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+}
