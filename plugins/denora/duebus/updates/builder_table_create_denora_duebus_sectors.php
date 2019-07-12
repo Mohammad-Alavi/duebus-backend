9f68d9493b9ccce44a3bde56dc029f258e8fe058
Moslem Deris <moslem.deris@gmail.com>
2019-07-12 14:08:03 +0430

    Create `Sectors` table

---
+++ plugins/denora/duebus/updates/builder_table_create_denora_duebus_sectors.php
@@ -0,0 +1,18 @@
+<?php namespace Denora\Duebus\Updates;
+
+use October\Rain\Database\Updates\Migration;
+use Schema;
+
+class BuilderTableCreateDenoraDuebusSectors extends Migration {
+    public function up() {
+        Schema::create('denora_duebus_sectors', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('label');
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('denora_duebus_sectors');
+    }
+}
