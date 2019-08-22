0139e285aaccb9d8d57ab8b60e9657522a00d6e3
Moslem Deris <moslem.deris@gmail.com>
2019-08-22 14:22:15 +0430

    Modify .htaccess to solve CORS problem

--- plugins/offline/cors/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'supportsCredentials' => true,
-    'maxAge'              => 3600,
-    'allowedOrigins'      => ['*'],
-    'allowedHeaders'      => ['*'],
-    'allowedMethods'      => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    'exposedHeaders'      => [''],
-];
