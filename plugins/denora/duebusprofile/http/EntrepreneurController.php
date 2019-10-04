f05fce3e547d033df9beb475302f4982d1468615
Moslem Deris <moslem.deris@gmail.com>
2019-10-05 01:06:55 +0330

    Minor changes

--- plugins/denora/duebusprofile/http/EntrepreneurController.php
+++ plugins/denora/duebusprofile/http/EntrepreneurController.php
@@ -24,7 +24,37 @@ class EntrepreneurController extends Controller {
 
         if (Auth::user()->entrepreneur) return Response::make(['You already are an entrepreneur'], 400);
 
-        $entrepreneur = $entrepreneurRepository->createEntrepreneur(Auth::user()->id);
+        $data = Request::all();
+
+        $validator = Validator::make($data, [
+            'educations'  => 'json',
+            'experiences' => 'json',
+        ]);
+
+        if ($validator->fails())
+            return Response::make($validator->messages(), 400);
+
+        //  Validate educations json
+        $hasEducations = array_has($data, 'educations');
+        if ($hasEducations) {
+            $educations = $data['educations'];
+            $educationsValidation = $this->validateEducationsJson($educations);
+            if ($educationsValidation->fails()) return Response::make($educationsValidation->messages(), 400);
+        }
+
+        //  Validate experiences json
+        $hasExperiences = array_has($data, 'experiences');
+        if ($hasExperiences) {
+            $experiences = $data['experiences'];
+            $experiencesValidation = $this->validateExperiencesJson($experiences);
+            if ($experiencesValidation->fails()) return Response::make($experiencesValidation->messages(), 400);
+        }
+
+        $entrepreneur = $entrepreneurRepository->createEntrepreneur(
+            Auth::user()->id,
+            $hasEducations ? $data['educations'] : null,
+            $hasExperiences ? $data['experiences'] : null
+        );
 
         return ProfileTransformer::transform($entrepreneur->user);
     }
@@ -39,21 +69,24 @@ class EntrepreneurController extends Controller {
         $data = Request::all();
 
         $validator = Validator::make($data, [
-            'educations' => 'json',
+            'educations'  => 'json',
+            'experiences' => 'json',
         ]);
 
         if ($validator->fails())
             return Response::make($validator->messages(), 400);
 
         //  Validate educations json
-        if (array_has($data, 'educations')) {
+        $hasEducations = array_has($data, 'educations');
+        if ($hasEducations) {
             $educations = $data['educations'];
             $educationsValidation = $this->validateEducationsJson($educations);
             if ($educationsValidation->fails()) return Response::make($educationsValidation->messages(), 400);
         }
 
         //  Validate experiences json
-        if (array_has($data, 'experiences')) {
+        $hasExperiences = array_has($data, 'experiences');
+        if ($hasExperiences) {
             $experiences = $data['experiences'];
             $experiencesValidation = $this->validateExperiencesJson($experiences);
             if ($experiencesValidation->fails()) return Response::make($experiencesValidation->messages(), 400);
@@ -89,10 +122,10 @@ class EntrepreneurController extends Controller {
         $data = ['data' => json_decode($json, true)];
 
         return Validator::make($data, [
-            'data.*.company'         => 'required|string|min:2',
+            'data.*.company'   => 'required|string|min:2',
             'data.*.job_title' => 'required|string|min:2',
-            'data.*.from'           => 'required|date',
-            'data.*.to'             => 'required|date',
+            'data.*.from'      => 'required|date',
+            'data.*.to'        => 'required|date',
         ]);
     }
 
