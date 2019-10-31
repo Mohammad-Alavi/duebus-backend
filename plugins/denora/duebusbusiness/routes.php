182e6b49fe59434ee73a20c325dedb6fb33dad7a
Moslem Deris <moslem.deris@gmail.com>
2019-10-31 17:57:47 +0330

    Make `getBusinesses` route available for guest users

--- plugins/denora/duebusbusiness/routes.php
+++ plugins/denora/duebusbusiness/routes.php
@@ -1,11 +1,11 @@
 <?php
 
 Route::group(['prefix' => 'api/v1'], function () {
-//    Route::resource('businesscontrollers', 'Denora\Duebusbusiness\Http\BusinessController');
+
+    Route::resource('business', 'Denora\Duebusbusiness\Http\BusinessController');
 
     Route::middleware(['jwt.auth'])->group(
         function () {
-            Route::resource('business', 'Denora\Duebusbusiness\Http\BusinessController');
             Route::resource('business/view', 'Denora\Duebusbusiness\Http\ViewController');
             Route::resource('business/reveal', 'Denora\Duebusbusiness\Http\RevealController');
             Route::resource('bookmark', 'Denora\Duebusbusiness\Http\BookmarkController');
