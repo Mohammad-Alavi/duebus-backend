2c645e69bd03b22684fb27000d55d09a8474d072
Moslem Deris <moslem.deris@gmail.com>
2019-11-01 16:40:00 +0330

    Add new events

--- plugins/denora/notification/classes/BaseEvent.php
+++ plugins/denora/notification/classes/BaseEvent.php
@@ -1,6 +1,7 @@
 <?php namespace Denora\Notification\Classes;
 
 use Denora\Duebusprofile\Classes\Repositories\UserRepository;
+use Denora\Notification\Classes\Channels\OneSignalChannel;
 use Denora\Notification\Classes\Channels\SaveToDatabaseChannel;
 
 abstract class BaseEvent
@@ -11,6 +12,8 @@ abstract class BaseEvent
     static $ACTION_TYPE_INVESTOR_PROFILE_UNVERIFIED = 'investor_profile_unverified';
     static $ACTION_TYPE_BUSINESS_CREATED = 'business_created';
     static $ACTION_TYPE_BUSINESS_PUBLISHED = 'business_published';
+    static $ACTION_TYPE_BUSINESS_VIEWED = 'business_viewed';
+    static $ACTION_TYPE_BUSINESS_REVEALED = 'business_revealed';
 
     static $OBJECT_TYPE_BUSINESS = 'business';
 
@@ -21,14 +24,15 @@ abstract class BaseEvent
     public $objectId = null;
     public $cost = null;
 
-    public abstract function getActionType();
-
     public function __construct(int $receiverUserId)
     {
         $this->receiverUser = (new UserRepository())->findById($receiverUserId);
 
         new SaveToDatabaseChannel($this);
+        new OneSignalChannel($this);
 
     }
+
+    public abstract function getActionType();
 
 }
