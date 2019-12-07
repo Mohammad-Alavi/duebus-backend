7cfe0d37e1488b181db36415406780ad0c9c1699
Moslem Deris <moslem.deris@gmail.com>
2019-12-07 17:18:43 +0330

    Fix a bug of type in sessions

--- plugins/denora/inbox/classes/repositories/SessionRepository.php
+++ plugins/denora/inbox/classes/repositories/SessionRepository.php
@@ -59,7 +59,10 @@ class SessionRepository
     static public function paginate(int $page, int $userId, $type = null, $business_id = null)
     {
         $query = Session::query();
-        $query->where('sender_id', $userId)->orWhere('receiver_id', $userId);
+        $query->where(function ($query) use ($userId) {
+            $query->where('sender_id', $userId)
+                ->orWhere('receiver_id', $userId);
+        });
         if ($type) $query->where('type', $type);
         if ($business_id) $query->where('business_id', $business_id);
         $query->orderByDesc('updated_at');
